810f7764fd32531ce3da6df132aad4d0e525fad8
SinanSezer <sinaan.sezer1@gmail.com>
2022-10-15 13:37:15 +0300

    Initial Commit

---
+++ Odev7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Odev7
+//
+//  Created by Sinan Sezer on 15.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
